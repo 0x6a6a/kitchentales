c6bc0e331bb9fa36402d2265fdb8d158ab425c84
Tim Weber <scy@scy.name>
2021-05-03 21:44:31 +0200

    Add favicon

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,4 +6,5 @@ outdir='dist'
 mkdir -p "$outdir"
 ./scripts/build-hls.sh
 convert assets/background.webp -colorspace Gray -resize 1920x1080 -quality 75 "$outdir/background.jpg"
+cp -av assets/favicon.png "$outdir/favicon.ico"
 cp -av src/index.html src/kitchentales.css src/kitchentales.js "$outdir"
